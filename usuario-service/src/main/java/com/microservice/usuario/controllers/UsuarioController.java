a78de37562585a3828983725ee0233e823772d1e
Daniel Penelva <d4n.andrade@gmail.com>
2024-03-14 04:33:54 -0300

    restTemplate - criando os métodos getCarros e getMotos para UsuarioService e para o UsuarioController

--- usuario-service/src/main/java/com/microservice/usuario/controllers/UsuarioController.java
+++ usuario-service/src/main/java/com/microservice/usuario/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.microservice.usuario.controllers;
 
 import com.microservice.usuario.entities.Usuario;
+import com.microservice.usuario.models.Carro;
+import com.microservice.usuario.models.Moto;
 import com.microservice.usuario.services.UsuarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -40,5 +42,42 @@ public class UsuarioController {
     @PostMapping("/save")
     public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
         return ResponseEntity.ok(usuarioService.save(usuario));
+    }
+
+
+    /* Métodos do controller que fará comunicação com os microserviços Carro e Moto - utilizando RestTemplate */
+
+    /**
+     * Método para obter a lista de carros associados a um usuário.
+     * @param usuarioId O ID do usuário para o qual se deseja obter os carros.
+     * @return Um ResponseEntity contendo uma lista de objetos do tipo Carro associados ao usuário.
+     */
+
+    // http://localhost:8001/api/usuario/carros/{usuarioId}
+    @GetMapping("/carros/{usuarioId}")
+    public ResponseEntity<List<Carro>> getCarros(@PathVariable int usuarioId) {
+        Usuario usuario = usuarioService.getUsuarioById(usuarioId);
+        if (usuario == null) {
+            return ResponseEntity.notFound().build();
+        }
+        List<Carro> carros = usuarioService.getCarros(usuarioId);
+        return ResponseEntity.ok(carros);
+    }
+
+    /**
+     * Método para obter a lista de motos associadas a um usuário.
+     * @param usuarioId O ID do usuário para o qual se deseja obter as motos.
+     * @return Um ResponseEntity contendo uma lista de objetos do tipo Moto associadas ao usuário.
+     */
+
+    // http://localhost:8001/api/usuario/motos/{usuarioId}
+    @GetMapping("/motos/{usuarioId}")
+    public ResponseEntity<List<Moto>> getMotos(@PathVariable int usuarioId) {
+        Usuario usuario = usuarioService.getUsuarioById(usuarioId);
+        if (usuario == null) {
+            return ResponseEntity.notFound().build();
+        }
+        List<Moto> motos = usuarioService.getMotos(usuarioId);
+        return ResponseEntity.ok(motos);
     }
 }
--- usuario-service/src/main/java/com/microservice/usuario/services/UsuarioService.java
+++ usuario-service/src/main/java/com/microservice/usuario/services/UsuarioService.java
@@ -1,9 +1,12 @@
 package com.microservice.usuario.services;
 
 import com.microservice.usuario.entities.Usuario;
+import com.microservice.usuario.models.Carro;
+import com.microservice.usuario.models.Moto;
 import com.microservice.usuario.repositories.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,6 +15,7 @@ import java.util.List;
 public class UsuarioService {
 
     private UsuarioRepository usuarioRepository;
+    private RestTemplate restTemplate;
 
     public List<Usuario> getAll() {
         return usuarioRepository.findAll();
@@ -23,5 +27,28 @@ public class UsuarioService {
 
     public Usuario save(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+
+    /* Métodos do serviço que fará comunicação com os microserviços Carro e Moto - utilizando RestTemplate */
+
+    /**
+     * Método para obter a lista de carros associados a um usuário.
+     * @param usuarioId O ID do usuário para o qual se deseja obter os carros.
+     * @return Uma lista de objetos do tipo Carro associados ao usuário.
+     */
+    public List<Carro> getCarros(int usuarioId) {
+        List<Carro> carros = restTemplate.getForObject("http://localhost:8002/api/carro/searchUserId/" + usuarioId, List.class);
+        return carros;
+    }
+
+    /**
+     * Método para obter a lista de motos associadas a um usuário.
+     * @param usuarioId O ID do usuário para o qual se deseja obter as motos.
+     * @return Uma lista de objetos do tipo Moto associadas ao usuário.
+     */
+    public List<Moto> getMotos(int usuarioId) {
+        List<Moto> motos = restTemplate.getForObject("http://localhost:8003/api/moto/searchUserId/" + usuarioId, List.class);
+        return motos;
     }
 }
