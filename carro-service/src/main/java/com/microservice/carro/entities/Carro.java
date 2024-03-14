ac6b48129b1e42b4c27ed9366567b00b35d544bc
Daniel Penelva <d4n.andrade@gmail.com>
2024-03-14 03:35:35 -0300

    carro-service - Criando entidade Carro e o repositorio CarroRepository + criando arquivo de configuração (application.properties)

---
+++ carro-service/src/main/java/com/microservice/carro/entities/Carro.java
@@ -0,0 +1,25 @@
+package com.microservice.carro.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String marca;
+    private String modelo;
+    private int usuarioId;
+}
