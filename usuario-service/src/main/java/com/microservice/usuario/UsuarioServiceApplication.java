e9cc7a100b56d6aae161e420eaec6ab4b0a85a2f
Daniel Penelva <d4n.andrade@gmail.com>
2024-03-14 14:49:27 -0300

    open-feign - add dependencia OpenFeign + add anotação @EnableFeignClients

--- usuario-service/src/main/java/com/microservice/usuario/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/microservice/usuario/UsuarioServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
